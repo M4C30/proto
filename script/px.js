6464c57b779ec8e7852022acb00c1ca0c5f79d5a
Mehtthewdh <mehdehaan13@gmail.com>
2022-04-24 15:14:08 +0200

    px

--- script/px.js
+++ script/px.js
@@ -1,27 +1,21 @@
 const container = document.querySelector('.container');
-let size = 50;
 const color = document.querySelector('.color');
 
-let timeOut;
+function addTile() {
 
-function addTile(size) {
-
-  for (let i = 0; i < size * size; i++) {
+  for (let i = 0; i < 50 * 50; i++) {
     const div = document.createElement('div');
-    div.classList.add('pixel');
 
     div.addEventListener('click', () => {
-        // inplaats van click kan je ook mousedown gebruiken
+
         if (confirm("Confirm your tile") == true) {
             div.style.backgroundColor = color.value;
-            timeOut = setTimeout(addTile, 10000);
-        } else {
-            div.style.backgroundColor = none;
-        }
+            setTimeout(addTile, 10000);
+        } else {}
     })
 
     container.appendChild(div);
   }
 }
 
-addTile(size);
+addTile();
